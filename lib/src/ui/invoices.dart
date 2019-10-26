8809c8baf1c70dfafee41081e0cf3c37be7f2150
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-26 19:11:40 -0300

    refactoring

--- lib/src/ui/invoices.dart
+++ lib/src/ui/invoices.dart
@@ -19,7 +19,7 @@ class _InvoicesState extends State<Invoices> {
   void initState() {
     widget._invoiceBloc.init();
     widget._invoiceBloc.fetchAllInvoices();
-    updateTitle(0);
+    _updateTitle(0);
     super.initState();
   }
 
@@ -33,44 +33,53 @@ class _InvoicesState extends State<Invoices> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: title,
-        leading: Icon(Icons.event),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(hintText: "Search"),
-            ),
-            Expanded(
-                child: PageView.builder(
-              onPageChanged: (int page) => updateTitle(page),
-              reverse: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int page) {
-                return buildStreamBuilder(page);
-              },
-            ))
-          ],
-        ),
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: title,
+      leading: Icon(Icons.event),
+    );
+  }
+
+  Container _buildBody() {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[_buildSeachTextField(), _buildList()],
       ),
     );
   }
 
-  StreamBuilder<InvoiceResult> buildStreamBuilder(int page) {
+  TextField _buildSeachTextField() {
+    return TextField(
+      decoration: InputDecoration(hintText: "Search"),
+    );
+  }
+
+  Expanded _buildList() {
+    return Expanded(
+        child: PageView.builder(
+      onPageChanged: (int page) => _updateTitle(page),
+      reverse: true,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int page) {
+        return _buildStreamBuilder(page);
+      },
+    ));
+  }
+
+  StreamBuilder<InvoiceResult> _buildStreamBuilder(int page) {
     return StreamBuilder(
       stream: widget._invoiceBloc.allInvoices,
       builder: (context, AsyncSnapshot<InvoiceResult> snapshot) {
-        if (snapshot.hasData || invoiceResult != null) {
-          if (snapshot.data != null) invoiceResult = snapshot.data;
-          return ListView.builder(
-            itemCount: invoiceResult.total,
-            itemBuilder: (BuildContext context, int index) {
-              return buildInvoiceItem(index, page);
-            },
-          );
+        bool canBuildListView = snapshot.hasData || invoiceResult != null;
+        if (canBuildListView) {
+          if (snapshot.hasData) invoiceResult = snapshot.data;
+          return _buildInvoiceListView(page);
         } else if (snapshot.hasError) {
           return Text(snapshot.error.toString());
         }
@@ -79,7 +88,16 @@ class _InvoicesState extends State<Invoices> {
     );
   }
 
-  ListTile buildInvoiceItem(int index, int page) {
+  ListView _buildInvoiceListView(int page) {
+    return ListView.builder(
+      itemCount: invoiceResult.total,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildInvoiceItem(context, index, page);
+      },
+    );
+  }
+
+  ListTile _buildInvoiceItem(BuildContext context, int index, int page) {
     return ListTile(
       title: Text(invoiceResult.results[index].description),
       subtitle: Text(invoiceResult.results[index].valueFormated),
@@ -87,22 +105,53 @@ class _InvoicesState extends State<Invoices> {
         invoiceResult.results[index].dayDue.toString(),
         style: TextStyle(fontSize: 16),
       ),
-      onLongPress: () {},
-      leading: Icon(
-          invoiceResult.results[index].checkIfWasPayed(getNewPaymentDate(page))
-              ? Icons.check
-              : Icons.error),
+      onLongPress: () => _showInvoiceMenu(context),
+      leading: _buildIcon(index, page),
     );
   }
 
-  void updateTitle(int page) {
+  void _showInvoiceMenu(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            child: new Wrap(
+              children: <Widget>[
+                new ListTile(
+                    leading: new Icon(Icons.check),
+                    title: new Text('Flag payment'),
+                    onTap: () => {}),
+                new ListTile(
+                  leading: new Icon(Icons.edit),
+                  title: new Text('Edit'),
+                  onTap: () => {},
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.delete),
+                  title: new Text('Delete'),
+                  onTap: () => {},
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  Icon _buildIcon(int index, int page) {
+    return Icon(
+        invoiceResult.results[index].checkIfWasPayed(_getNewPaymentDate(page))
+            ? Icons.check
+            : Icons.error);
+  }
+
+  void _updateTitle(int page) {
     setState(() {
-      var paymentDate = getNewPaymentDate(page);
+      var paymentDate = _getNewPaymentDate(page);
       title = Text("Payments of ${paymentDate.month}/${paymentDate.year}");
     });
   }
 
-  DateTime getNewPaymentDate(int page) {
+  DateTime _getNewPaymentDate(int page) {
     var dateNow = DateTime.now();
     return DateTime(dateNow.year, dateNow.month - page, dateNow.day);
   }
